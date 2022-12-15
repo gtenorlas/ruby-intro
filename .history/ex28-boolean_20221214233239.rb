a1844c686f82675ceb1824003cb773236711906d
Gene Tenorlas <gene.tenorlas@yahoo.com>
2022-12-15 04:34:00 +0000

    Create ex28 boolean expression

---
+++ .history/ex28-boolean_20221214233239.rb
@@ -0,0 +1,20 @@
+puts true && true
+puts false && true
+puts 1 == 1 && 2 == 1
+puts "test" == "test"
+puts 1 == 1 || 2 != 1
+puts true && 1 == 1
+puts false && 0 != 0
+puts true || 1 == 1
+"test" == "testing"
+1 != 0 && 2 == 1
+"test" != "testing"
+"test" == 1
+!(true && false)
+!(1 == 1 && 0 != 1)
+!(10 == 1 || 1000 == 1000)
+!(1 != 10 || 3 == 4)
+!("testing" == "testing" && "Zed" == "Cool Guy")
+1 == 1 && (!("testing" == 1 || 1 == 0))
+"chunky" == "bacon" && (!(3 == 4 || 3 == 3))
+3 == 3 && (!("testing" == "testing" || "Ruby" == "Fun"))
